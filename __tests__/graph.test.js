a9cdba3e885028756c432a3c998bb8e236c69134
Aishahsatouf <aishasattouf1996@gmail.com>
2021-02-13 20:17:00 +0200

    chal 35

---
+++ __tests__/graph.test.js
@@ -0,0 +1,46 @@
+const [Node,Graph]=require('../Data-structure/graph/graph');
+
+const graph = new Graph();
+
+const two = new Node(2);
+const three = new Node(3);
+const six = new Node(6);
+const seven = new Node(7);
+const eight = new Node(8);
+
+// graph.addNode(two);
+// graph.addNode(three);
+// graph.addNode(six);
+// graph.addNode(seven);
+// graph.addNode(eight);
+
+// graph.addDirectedEdge(two, six);
+// graph.addDirectedEdge(two, three);
+// graph.addDirectedEdge(three, seven);
+// graph.addDirectedEdge(eight, two);
+// graph.addDirectedEdge(three, six);
+
+describe('implementation of graphs',()=>{
+    it('add a node to the graph',()=>{
+      graph.addNode(two); 
+      expect(graph.size()).toEqual(1); 
+    });
+    it('add an edge successfuly and return a collection of all nodes as a list ',()=>{
+        graph.addNode(three);
+        graph.addDirectedEdge(two, three);
+        expect(graph.size()).toEqual(2); 
+        expect(graph.getAll()).toEqual(['node-->{"value":2}',
+        'edge-->[{"vertex":{"value":3}}]',
+        'node-->{"value":3}',
+        'edge-->[]'
+    ]);
+    });
+    it('retrives all the neighbors of a node ',()=>{
+      graph.addNode(six);
+      graph.addDirectedEdge(two, six);
+      expect(JSON.stringify(graph.getNeighbours(two))).toEqual('[{"vertex":{"value":3}},{"vertex":{"value":6}}]')
+    });
+    it ('will return the size of a graph',()=>{
+        expect(graph.size()).toEqual(3);
+    });
+});
